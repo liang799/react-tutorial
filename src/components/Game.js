da67b3dd335f4b18fbb9bc236ba0e26b45753a6f
liang799 <71926348+liang799@users.noreply.github.com>
2022-09-13 10:29:48 +0800

    Bold current status

--- src/components/Game.js
+++ src/components/Game.js
@@ -57,11 +57,20 @@ export default class Game extends React.Component {
       const desc = move
         ? "Go to move #" + move + " ("+step.coordinates+")"
         : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
+      if (move === this.state.stepNumber) {
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}><b>{desc}</b></button>
+          </li>
+        );
+      } else {
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      }
+
     });
     let status;
     if (winner) {
